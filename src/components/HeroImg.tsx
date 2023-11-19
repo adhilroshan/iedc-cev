fd5064c95e00cd1980657e98d07a0f53380fa98f
adhilroshan <adhilchra@gmail.com>
2023-11-19 15:16:26 +0530

    Refactor HeroImg component to use Next.js Image component

--- src/components/HeroImg.tsx
+++ src/components/HeroImg.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import Image from "next/image";
 
 const HeroImg = () => {
   return (
-    <div className=''>
-      <img
-        className='w-full rounded-lg h-full object-cover'
-        src='https://assets.website-files.com/64ae767417b9b300d1f932fa/64b42e4cfb16b8a6075048d0_home_02-p-500.jpg'
-        alt=''
+    <div className="">
+      <Image
+        className="w-full rounded-lg h-full object-cover"
+        src="https://assets.website-files.com/64ae767417b9b300d1f932fa/64b42e4cfb16b8a6075048d0_home_02-p-500.jpg"
+        alt=""
       />
     </div>
-  )
-}
+  );
+};
 
-export default HeroImg
+export default HeroImg;
