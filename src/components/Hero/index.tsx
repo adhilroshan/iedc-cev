1f754676a86bf5ab937018559469dbabdb23a5c8
Adhil Roshan <58517896+adhilroshan@users.noreply.github.com>
2024-03-31 01:44:48 +0530

    upgraded ui

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,14 @@
+export default function index() {
+  return (
+    <div className="h-screen  justify-end flex flex-col">
+      <div className="pb-20 pl-10 gap-3 flex flex-col">
+        <h1 className="text-6xl font-bold ">
+          Innovation and Entrepreneurship Development Centre
+        </h1>
+        <p className="overflow-clip w-[40vw] font-light text-xl ">
+          College of Engineering Vadakara.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Navbar = () => {
-  return (
-    <nav className='absolute drop-shadow-lg bg-purple-400 -top-6 z-20 left-0 right-0 w-[80vw] h-16   rounded-full mx-auto'></nav>
-  )
-}
-
-export default Navbar
